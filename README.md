bb2f58e3b7021e2b595ef3a7ec37c461dcc56c6e
Slim Jallouli <41168224+Slim-ST@users.noreply.github.com>
2022-03-07 11:37:36 -0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # STM32_AWS_DashBoard
 Demo on how to use JavaScript to connect to AWS and exchange MQTT messages
+
+[B-U585I-IOT02A](https://www.st.com/en/evaluation-tools/b-u585i-iot02a.html)
+
+[Source code](https://github.com/FreeRTOS/lab-iot-reference-stm32u5)
+
+[Create an IAM user in your AWS account](https://docs.aws.amazon.com/IAM/latest/UserGuide/id_users_create.html)
+
+- Open STM32_AWS_DashBoard\assets\iot.js and update the following
+
+```
+// WARNING!!! It is not recommended to expose
+// sensitive credential information in code.
+// Consider setting the following AWS values from a secure source.
+
+// example: us-east-1
+const REGION = "us-east-2";
+// example: xzy-ats.iot.your-region.amazonaws.com
+const IOT_ENDPOINT = "a1qwhobjtvew8t-ats.iot.us-east-2.amazonaws.com";
+// your AWS access key ID
+const KEY_ID = "";
+// your AWS secret access key
+const SECRET_KEY = "";
+```
+
+- Open Index.html
+- Replave **SLIM_U5_AMZ** with your device name
+- Click "Subscribe"
---
+++ assets/iot.js
@@ -0,0 +1,255 @@
+// WARNING!!! It is not recommended to expose
+// sensitive credential information in code.
+// Consider setting the following AWS values from a secure source.
+
+// The device ID
+let DeviceID = '';
+// example: us-east-1
+const REGION = "us-east-2";
+// example: xzy-ats.iot.your-region.amazonaws.com
+const IOT_ENDPOINT = "a1qwhobjtvew8t-ats.iot.us-east-2.amazonaws.com";
+// your AWS access key ID
+const KEY_ID = "";
+// your AWS secret access key
+const SECRET_KEY = "";
+	
+let client = {};
+
+let subscribed = false;
+let env_sensor_topic  = "/"+DeviceID+"/env_sensor_data";
+let motion_sensor_topic = "/"+DeviceID+"/motion_sensor_data";
+let shadow_topic = "$aws/things/"+DeviceID+"/shadow/update";
+var _clientToken = Math.random() * 1000000;
+
+//gets MQTT client
+function initClient()
+{
+  const clientId = Math.random().toString(36).substring(7);
+  const _client  = new Paho.MQTT.Client(getEndpoint(), clientId);
+
+    // publish method added to simplify messaging
+    _client.publish = function(topic, payload)
+	{
+        let payloadText = JSON.stringify(payload);
+        let message = new Paho.MQTT.Message(payloadText);
+        message.destinationName = topic;
+        message.qos = 0;
+        _client.send(message);
+    }
+
+    return _client;
+}
+
+function getEndpoint()
+{
+// WARNING!!! It is not recommended to expose
+// sensitive credential information in code.
+// Consider setting the following AWS values
+// from a secure source.
+
+    // example: us-east-1
+    //const REGION = "us-east-2";
+
+    // example: blahblahblah-ats.iot.your-region.amazonaws.com
+    //const IOT_ENDPOINT = "a1qwhobjtvew8t-ats.iot.us-east-2.amazonaws.com";
+
+    // your AWS access key ID
+    //const KEY_ID = 
+
+    // your AWS secret access key
+    //const SECRET_KEY = "";
+
+    // date & time
+    const dt = (new Date()).toISOString().replace(/[^0-9]/g, "");
+    const ymd = dt.slice(0,8);
+    const fdt = `${ymd}T${dt.slice(8,14)}Z`
+
+    const scope = `${ymd}/${REGION}/iotdevicegateway/aws4_request`;
+    const ks = encodeURIComponent(`${KEY_ID}/${scope}`);
+
+	let qs = `X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=${ks}&X-Amz-Date=${fdt}&X-Amz-SignedHeaders=host`;
+
+	const req = `GET\n/mqtt\n${qs}\nhost:${IOT_ENDPOINT}\n\nhost\n${p4.sha256('')}`;
+
+	qs += '&X-Amz-Signature=' + p4.sign(p4.getSignatureKey( SECRET_KEY, ymd, REGION, 'iotdevicegateway'), `AWS4-HMAC-SHA256\n${fdt}\n${scope}\n${p4.sha256(req)}`);
+    return `wss://${IOT_ENDPOINT}/mqtt?${qs}`;
+}
+
+function p4(){}
+p4.sign = function(key, msg)
+{
+    const hash = CryptoJS.HmacSHA256(msg, key);
+    return hash.toString(CryptoJS.enc.Hex);
+};
+
+p4.sha256 = function(msg)
+{
+    const hash = CryptoJS.SHA256(msg);
+    return hash.toString(CryptoJS.enc.Hex);
+};
+
+p4.getSignatureKey = function(key, dateStamp, regionName, serviceName)
+{
+    const kDate    = CryptoJS.HmacSHA256(dateStamp     , 'AWS4' + key);
+    const kRegion  = CryptoJS.HmacSHA256(regionName    , kDate);
+    const kService = CryptoJS.HmacSHA256(serviceName   , kRegion);
+    const kSigning = CryptoJS.HmacSHA256('aws4_request', kService);
+    return kSigning;
+};
+
+function connected()
+{
+  console.log("connected to AWS");
+  document.getElementById("idTitle").innerHTML = 'Connected to AWS';
+
+  HideShowElement("idButtonSubscribe", true);
+}
+
+function getClient(success)
+{
+    if (!success) success = ()=> connected();
+
+    const _client = initClient();
+
+    const connectOptions =
+	{
+      useSSL: true,
+      timeout: 3,
+      mqttVersion: 4,
+      onSuccess: success
+    };
+
+    _client.connect(connectOptions);
+
+    return _client;
+}
+
+function HideShowElement(elementID, HideShow) 
+{
+    var x = document.getElementById(elementID);
+
+    if(HideShow == true)
+    {
+        x.style.display = "block";
+    } 
+    else 
+    {
+      x.style.display = "none";
+    }
+}
+
+/* Unsubscribe from all topics */
+function unsubscribe()
+{
+    if(subscribed == true)
+    {
+        client.unsubscribe(env_sensor_topic);
+	    client.unsubscribe(motion_sensor_topic);
+        client.unsubscribe(shadow_topic+"/accepted");
+
+        subscribed = false;
+    }
+}
+
+/* Update the topics and subscribe */
+function subscribe()
+{
+    unsubscribe();
+
+    DeviceID = document.getElementById("idDeviceID").value ;
+
+    env_sensor_topic  = "/"+DeviceID+"/env_sensor_data";
+    motion_sensor_topic = "/"+DeviceID+"/motion_sensor_data";
+    shadow_topic = "$aws/things/"+DeviceID+"/shadow/update";
+
+    client.subscribe(env_sensor_topic);
+	client.subscribe(motion_sensor_topic);
+    client.subscribe(shadow_topic+"/accepted");
+
+    subscribed = true;
+
+    console.log("subscribed to:");
+    console.log(env_sensor_topic);
+    console.log(motion_sensor_topic);
+    console.log(shadow_topic);
+}
+
+function init()
+{
+    DeviceID = document.getElementById("idDeviceID").value ;
+
+    HideShowElement('idButtonSubscribe', false);
+
+    client = getClient(() => {
+        subscribe();
+	});
+	
+    client.onMessageArrived = processMessage;
+	
+	client.onConnection = connected();
+
+    client.onConnectionLost = function(e)
+	{
+        console.log(e);
+        document.getElementById("idTitle").innerHTML = 'Disconnected from AWS';
+        HideShowElement("idButtonSubscribe", false);
+    }
+}
+
+function processMessage(message)
+{
+    let info = JSON.parse(message.payloadString);
+	
+    console.log(info);
+	
+    if(message.destinationName == shadow_topic+"/accepted")
+    {
+        console.log(info);
+
+        try
+        {
+           if(info.state.reported.powerOn == '1')
+            {
+                document.getElementById("ImageBulb").src="./assets/pic_bulbon.gif";
+            }
+            else
+            {
+                document.getElementById("ImageBulb").src="./assets/pic_bulboff.gif";
+            }
+        }
+        catch
+        {
+            //Do nothing
+        }
+    }
+    else if(message.destinationName == env_sensor_topic)
+    {
+      add_env_sensor_data(info.temp_0_c, info.rh_pct);
+    }
+    else if(message.destinationName == motion_sensor_topic)
+    {
+        add_motion_sensor_data(info.acceleration_mG.x, info.acceleration_mG.y,info.acceleration_mG.z);
+    }
+}
+
+/* Sensd a shadow message, use "1" or "0" to goggle the led on/off*/
+function SendMessage(message)
+{
+    _clientToken++;
+
+    const publishData =
+	{
+	  state:{
+	    desired:{
+          powerOn: message,
+        }
+	  },
+
+      clientToken: _clientToken.toFixed()
+
+	};
+		
+    client.publish(shadow_topic, publishData);
+}
+
+window.addEventListener('DOMContentLoaded', ()=>init());
